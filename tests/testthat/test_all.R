8174432d3d09403cc6509384ecaa6919204aec03
Vince <vince.reuter@gmail.com>
2018-12-08 16:41:59 -0500

    first unit tests for setff

--- tests/testthat/test_all.R
+++ tests/testthat/test_all.R
@@ -1,48 +1,22 @@
-# Unit tests
-library(GenomicDistributions)
+# test_setff.R
+# Tests of binding value to name.
+# Author: Vince Reuter
+# Email: vpr9v@virginia.edu
 
-context("Testthat context...")
+context("setff")
 
-test_that("featureDistribution",  {
-
-	queryFile = system.file("extdata", "setB_100.bed.gz", package="GenomicDistributions")
-	query = rtracklayer::import(queryFile)
-
-	featureExample = GenomicRanges::shift(query, round(rnorm(length(query), 0,1000)))
-	fdd = featureDistanceDistribution(query, featureExample)
-	featureFile = system.file("extdata", "vistaEnhancers.bed.gz", package="GenomicDistributions")
-	feats = rtracklayer::import(featureFile)
-
-	#' featureDistance = featureDistanceDistribution(query, feats)
-	#' expect_equal(sum(is.na(featureDistance)), -3)
-	#' expect_equal(sum(featureDistance, na.rm=TRUE), 743969)
+test_that("result of function created matches value provided",  {
+	ns = c("NEWPROCVAR", "RAND_DATA_VAR", "ProcData", "RawData", "ROUT.DIR", "PROCESSED.PROJECT")
+	xs = c("processed", "/home/data", "proc/data", "RawData", "output", "proc_proj")
+	if (length(ns) != length(xs)) stop(sprintf(
+		"%d names and %d values as test inputs", length(ns), length(xs)))
+	for (i in 1:length(ns)) {
+		expect_true(.isEmpty(optOrVar(!!ns[i])))
+		setff(ns[i], path = xs[i])
+		expect_equal(eval(get(paste0(.PDIRFUNCTAG, !!ns[i]))()), !!xs[i])
+	}
 })
 
- #' queryDT = GenomicDistributions:::grToDt(query)
- #'        featureDT = GenomicDistributions:::grToDt(features)
- #'        queryDTs = GenomicDistributions:::splitDataTable(queryDT, "chr")
- #'        featureDTs = GenomicDistributions:::splitDataTable(featureDT, "chr")
- #'        as.vector(unlist(mapply(queryDTs, featureDTs[names(queryDTs)], FUN=DTNearest)))
-
-
-
-test_that("Genome aggregate", {
-	queryFile = system.file("extdata", "vistaEnhancers.bed.gz", package="GenomicDistributions")
-	query = rtracklayer::import(queryFile)
-	# First, calculate the distribution:
-	x = aggregateOverGenomeBins(query, "hg19")
-	# Then, plot the result:
-	# plotGenomeAggregate(x)
-})
-
-
-
-test_that("Partitions", {
-	queryFile = system.file("extdata", "vistaEnhancers.bed.gz", package="GenomicDistributions")
-	query = rtracklayer::import(queryFile)
-	gp = genomicPartitions(query, "hg38")
-	gp = genomicPartitions(query, "hg19")
-	gp = genomicPartitions(query, "mm10")
-	gp = genomicPartitions(query, "mm9")
-	plotPartitions(gp)
-})
+#test_that("setting empty value is prohibited", {
+#
+#})
