18e8d3bb8c134e9fd7d6c048fe5db98b0a6fccab
Vince <vince.reuter@gmail.com>
2018-12-08 22:46:30 -0500

    implement another test set; cleanup vars and opts

--- tests/testthat/test_optOrVar.R
+++ tests/testthat/test_optOrVar.R
@@ -21,14 +21,16 @@ test_that("Option is recovered", {
     options(opts)
     if (is.null(getOption(n))) stop("Failed to set option: ", n)
     expect_equal(optOrVar(!!n), value)
-    opts[[n]] = NULL
+    opts = list(NULL)
+    names(opts) = n
     options(opts)
+    stopifnot(is.null(getOption(n)))
   }
 })
 
 test_that("Environment variable is recovered", {
   for (n in sapply(1:10, getRandVarName)) {
-    stopifnot(is.null(getOption(n)) && identical("", Sys.getenv(n)))  
+    stopifnot(is.null(getOption(n)) && identical("", Sys.getenv(n)))
     value = "dummy_test_value"
     args = list(value)
     names(args) = n
@@ -40,11 +42,43 @@ test_that("Environment variable is recovered", {
 })
 
 test_that("Option trumps environment variable", {
-
+  varnames = sapply(1:10, getRandVarName)
+  optValues = sapply(1:10, getRandVarName)
+  envVarValues = sapply(1:10, getRandVarName)
+  for (i in 1:10) {
+    n = varnames[i]
+    stopifnot(is.null(getOption(n)) && identical("", Sys.getenv(n)))
+    optVal = list(optValues[i])
+    names(optVal) = n
+    options(optVal)
+    envArg = list(envVarValues[i])
+    names(envArg) = n
+    do.call(Sys.setenv, args = envArg)
+    stopifnot(!is.null(getOption(n)) && !identical("", Sys.getenv(n)))
+    expect_equal(optOrVar(!!n), !!optValues[i])
+    Sys.unsetenv(n)
+    optVal = list(NULL)
+    names(optVal) = n
+    options(optVal)
+    stopifnot(is.null(getOption(n)) && identical("", Sys.getenv(n)))
+  }
 })
 
-test_that("NULL result when option or env. var. is empty", {
-
+test_that("NULL result when option is empty", {
+  varname = "dummy_test_opt"
+  for (x in list("", logical(0), character(0), numeric(0), integer(0))) {
+    stopifnot(is.null(getOption(varname)))
+    optArg = list(x)
+    names(optArg) = varname
+    options(optArg)
+    if (is.null(getOption(varname))) stop("Failed to set option: ", varname)
+    message("CLASS: ", class(optOrVar(varname)))
+    expect_true(is.null(optOrVar(varname)))
+    optArg = list(NULL)
+    names(optArg) = varname
+    options(optArg)
+    stopifnot(is.null(getOption(varname)))
+  }
 })
 
 test_that("Name for which to fetch value must be text", {
