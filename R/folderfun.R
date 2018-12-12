62d97ff36227f97a0d38876172efb338ac8f73cb
Vince <vince.reuter@gmail.com>
2018-12-12 10:37:11 -0500

    hide returned function output; close #13

--- R/folderfun.R
+++ R/folderfun.R
@@ -70,7 +70,7 @@ setff = function(name, path = NULL, pathVar = NULL) {
 	}
 	assign(funcName, tempFunc, envir=globalenv())
 	message("Created folder function ", funcName, "(): ", tempFunc())
-  return(tempFunc)
+  invisible(tempFunc)
 }
 
 
